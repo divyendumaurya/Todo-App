8dfec4ac01b4cc3486ac2032ded0bd645f6414ff
divyendumaurya <rajatmaurya0720@gmail.com>
2024-01-24 15:36:33 +0530

    Created input for todo- add task
    Created input box for task and add task function and got value from appJs file through props

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 
-function TodoInput() {
-    
-    
-  //  const handleAdd = (props)=>{
-  //     props.addList(inputText)
-  //       setInputText("");
-  //       return;
-  //   };
-
+function TodoInput(props) {
+    const [inputText,setInputText] = useState('');
+    const handleEnterPress = (e)=>{
+        if(e.keyCode===13){
+            props.addList(inputText)
+            setInputText("")
+        }
+    }
   return (
-    <div > 
+    <div className="input-container">
+      <input
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter your todo"
+        value={inputText}
+        onChange={e=>{
+            setInputText(e.target.value)
+        }}
+        onKeyDown={handleEnterPress}
+      />
+      <button className="add-btn" 
+      onClick={()=>{
+        props.addList(inputText)
+        setInputText("")
+      }}>+</button>      
     </div>
   );
-    
 }
 
 export default TodoInput;
