1b797aaf177b4b6757bda8297a995e7f2cd2569a
divyendumaurya <rajatmaurya0720@gmail.com>
2024-01-24 15:16:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import "./App.css"
+import TodoInput from './components/TodoInput'
+import Todolist from './components/TodoList';
+function App() {
+  
+  return (
+    <div className="main-container">
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TodoInput.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function TodoInput() {
+    
+    
+  //  const handleAdd = (props)=>{
+  //     props.addList(inputText)
+  //       setInputText("");
+  //       return;
+  //   };
+
+  return (
+    <div > 
+    </div>
+  );
+    
+}
+
+export default TodoInput;
