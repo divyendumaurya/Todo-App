3c4d8779f0103d63670c39bff71bbc6b2210ffa8
divyendumaurya <rajatmaurya0720@gmail.com>
2024-01-24 15:24:04 +0530

    initialized the app , added functions
    initialized the app , added functions for add and delete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css"
 import TodoInput from './components/TodoInput'
 import Todolist from './components/TodoList';
 function App() {
-  
+  const [listTodo,setListTodo]=useState([]);
+  let addList = (inputText)=>{
+    if(inputText!=='')
+      setListTodo([...listTodo,inputText]);
+  }
+  const deleteListItem = (index)=>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(index,1)
+    setListTodo([...newListTodo])
+  }
   return (
     <div className="main-container">
       
